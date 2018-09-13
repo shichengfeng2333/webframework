a466d7a642ce74de43e3a31bb3e5922066b5d739
shichengfeng <1448458830@qq.com>
2018-09-13 15:21:30 +0800

    石成峰hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Auther shi
+ * @Date 2018/9/13 15:20
+ * @Version 1.0
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
